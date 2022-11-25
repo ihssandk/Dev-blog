5a5e242ea51f3625e70d74cc15797b6d6b5bb29c
idyol <ihssandk@gmail.com>
2022-11-25 20:15:18 +0100

    header fix

--- components/Header.tsx
+++ components/Header.tsx
@@ -14,23 +14,23 @@ const Header = () => {
           .then((newCatgories : any)=> setCategories(newCatgories))
     }, [])
   return (
-    <div className ="container  mx-auto mb-8 px-5">
-        <div className=" w-full inline-block  py-8">
+    <div className ="block border-b-2 mb-8 SS">
+        <div className=" w-full inline-block ">
             <div className="md:float-left block">
                 <Link href ='/'>
                     <span className="cursor-pointer font-bold text-5xl  font-syncopate drop-shadow-lg shadow-black text-[#a2b4c6]">
                         <Image 
                         src={require('../public/logo.png')}
                         alt="digitalyos logo"
-                        width= {250}
-                        height={250}/>
+                        width= {200}
+                        height={200}/>
                     </span>
                 </Link>
 
             </div>
             <div className="hidden md:float-left md:contents ">
                 {categories.map((category)=>(<Link key={category.slug} href={`/category/${category.slug}`}>
-                    <span className="md:float-right mt-6 align-middle text-2xl text-[#c2c3c4] uppercase  hover:text-[#ffffff] ml-4 border-x-stone-600 font-bold cursor-pointer">{category.name}</span>
+                    <span className="md:float-right mt-6 align-middle text-2xl text-[#d6dbe0] uppercase  hover:text-[#a7beff] font-bold cursor-pointer">{category.name}</span>
                 </Link>))}
             </div>
 
