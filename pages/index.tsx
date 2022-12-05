acbbc5d16c81e42affbf084a8c06107854093be4
idyol <ihssandk@gmail.com>
2022-12-06 00:00:32 +0100

    fix articlewidget

--- pages/index.tsx
+++ pages/index.tsx
@@ -15,9 +15,8 @@ const Home: NextPage = ({articles} :any) => {
       <FeaturedArticles />
         <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
           <div className="lg:col-span-8 col-span-1">
-          {articles?.map((article: any, index: any)=>(
+          {articles.map((article: any, index: any)=>(
           <>
-
             <ArticleCard article={article.node} key={article.title}/>
           </>
           ))}
